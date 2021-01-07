0fe471c2170a8a7d40ca705271ba66cd19a84e1e
Fausto Rodrigo Vizuete Morales <67701116+favizuete@users.noreply.github.com>
2021-01-07 16:42:36 -0500

    IHS to String

---
+++ 06-Code/InventoryHadwareStoretoString/src/ec/edu/espe/inventory/model/Category.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author Fausto Vizuete ESPE-DCCO
+ */
+public class Category {
+    private String idCategory;
+    private String nameCategory;
+
+    public Category(String idCategory, String nameCategory) {
+        this.idCategory = idCategory;
+        this.nameCategory = nameCategory;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "idCategory=" + idCategory + ", nameCategory=" + nameCategory + '}';
+    }
+    
+    
+
+    /**
+     * @return the idCategory
+     */
+    public String getIdCategory() {
+        return idCategory;
+    }
+
+    /**
+     * @param idCategory the idCategory to set
+     */
+    public void setIdCategory(String idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    /**
+     * @return the nameCategory
+     */
+    public String getNameCategory() {
+        return nameCategory;
+    }
+
+    /**
+     * @param nameCategory the nameCategory to set
+     */
+    public void setNameCategory(String nameCategory) {
+        this.nameCategory = nameCategory;
+    }
+    
+}
