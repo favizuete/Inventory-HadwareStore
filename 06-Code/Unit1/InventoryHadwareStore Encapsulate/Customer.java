c2f6c738c153fbf41517d5ca84656e25aab9cd19
Fausto Rodrigo Vizuete Morales <67701116+favizuete@users.noreply.github.com>
2021-01-27 11:29:44 -0500

    Unit 1

---
+++ 06-Code/Unit1/InventoryHadwareStore Encapsulate/Customer.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author Fausto Vizuete ESPE-DCCO
+ */
+public class Customer {
+    private String name;
+    private String adress;
+    private int phonenumber;
+    private String mail;
+    private String id;
+
+    public Customer(String name, String adress, int phonenumber, String mail, String id) {
+        this.name = name;
+        this.adress = adress;
+        this.phonenumber = phonenumber;
+        this.mail = mail;
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the adress
+     */
+    public String getAdress() {
+        return adress;
+    }
+
+    /**
+     * @param adress the adress to set
+     */
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    /**
+     * @return the phonenumber
+     */
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    /**
+     * @param phonenumber the phonenumber to set
+     */
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    /**
+     * @return the mail
+     */
+    public String getMail() {
+        return mail;
+    }
+
+    /**
+     * @param mail the mail to set
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+}
---
+++ 06-Code/Unit1/InventoryHadwareStore Encapsulate/Product.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.inventory.model;
+
+/**
+ *
+ * @author Fausto Vizuete ESPE-DCCO
+ */
+public class Product {
+    private String id;
+    private String name;
+    private String brand;
+    private int quantity;
+    private float price;
+
+    public Product(String id, String name, String brand, int quantity, float price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+}
