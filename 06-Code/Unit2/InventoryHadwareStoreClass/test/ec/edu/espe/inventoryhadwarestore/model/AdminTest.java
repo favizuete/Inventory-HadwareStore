43088b6bab55989d4b6c6bde03112b2a452fa075
Pablo Yánez Alvear <pabloyanez09966@gmail.com>
2021-03-03 11:05:38 -0500

    Upload Diagram Class V2

--- 06-Code/Unit2/InventoryHadwareStoreClass/test/ec/edu/espe/inventoryhadwarestore/model/AdminTest.java
+++ 06-Code/Unit2/InventoryHadwareStoreClass/test/ec/edu/espe/inventoryhadwarestore/model/AdminTest.java
@@ -20,6 +20,22 @@ public class AdminTest {
     
     public AdminTest() {
     }
+
+    @org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
     
     @BeforeClass
     public static void setUpClass() {
@@ -40,38 +56,35 @@ public class AdminTest {
     /**
      * Test of toString method, of class Admin.
      */
-    @Test
+    @org.junit.Test
     public void testToString() {
         System.out.println("toString");
-        String expResult = "null";
-        String result = "null";
+        Admin instance = null;
+        String expResult = "";
+        String result = instance.toString();
         assertEquals(expResult, result);
-        if(result !=expResult){
-        
-          fail("The test case is a prototype.");
-        }
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
     /**
      * Test of getName method, of class Admin.
      */
-    @Test
+    @org.junit.Test
     public void testGetName() {
         System.out.println("getName");
         Admin instance = null;
-        String expResult = "Pablo";
-        String result = "PABLO";
+        String expResult = "";
+        String result = instance.getName();
         assertEquals(expResult, result);
-        if(result !=expResult){
-        
-          fail("The test case is a prototype.");
-        }
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
     /**
      * Test of setName method, of class Admin.
      */
-    @Test
+    @org.junit.Test
     public void testSetName() {
         System.out.println("setName");
         String name = "";
@@ -84,23 +97,21 @@ public class AdminTest {
     /**
      * Test of getPassword method, of class Admin.
      */
-    @Test
+    @org.junit.Test
     public void testGetPassword() {
         System.out.println("getPassword");
         Admin instance = null;
-        String expResult = "RICHARD123";
-        String result = "RICHARD123";
+        String expResult = "";
+        String result = instance.getPassword();
         assertEquals(expResult, result);
-         if(result !=expResult){
-        
-          fail("The test case is a prototype.");
-        }
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
     /**
      * Test of setPassword method, of class Admin.
      */
-    @Test
+    @org.junit.Test
     public void testSetPassword() {
         System.out.println("setPassword");
         String password = "";
@@ -113,23 +124,21 @@ public class AdminTest {
     /**
      * Test of getUser method, of class Admin.
      */
-    @Test
+    @org.junit.Test
     public void testGetUser() {
         System.out.println("getUser");
         Admin instance = null;
-        String expResult = "richard";
-        String result = "Richard";
+        String expResult = "";
+        String result = instance.getUser();
         assertEquals(expResult, result);
-        if(result !=expResult){
-        
-          fail("The test case is a prototype.");
-        }
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 
     /**
      * Test of setUser method, of class Admin.
      */
-    @Test
+    @org.junit.Test
     public void testSetUser() {
         System.out.println("setUser");
         String user = "";
