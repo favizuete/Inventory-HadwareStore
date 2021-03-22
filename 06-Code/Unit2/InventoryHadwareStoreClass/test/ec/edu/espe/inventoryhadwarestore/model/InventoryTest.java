03c367fc9363d9795db00a70f504f59f791cccb8
vanessazurita <vnzurita@espe.edu.ec>
2021-03-21 20:32:29 -0500

    UML

--- 06-Code/Unit2/InventoryHadwareStoreClass/test/ec/edu/espe/inventoryhadwarestore/model/InventoryTest.java
+++ 06-Code/Unit2/InventoryHadwareStoreClass/test/ec/edu/espe/inventoryhadwarestore/model/InventoryTest.java
@@ -66,7 +66,6 @@ public class InventoryTest {
         System.out.println("login");
         Admin admin = null;
         Inventory instance = new Inventory();
-        instance.login(admin);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
